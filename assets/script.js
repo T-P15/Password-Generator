bd8a0dd255214af60492293ac770cc61348855ce
mingosis <tom.piccirillo15@gmail.com>
2023-09-14 18:00:32 +1000

    generatePassword function complete

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,26 @@
 // Assignment code here
-var input = ["A", "B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","1","2","3","4","5","6","7","8","9","!","@","#","$","%","^","&","*","(",")"];
+function generatePassword() {
+    var key = '';
+    var inputs = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
+
+    for (let i = 1; i <= 12; i++) {
+        var character = Math.floor(Math.random()
+            * inputs.length);
+
+     key += inputs.charAt(character)
+    }
+
+    return key;
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword() 
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
